fc0ac2fad6d55c5952df5224485f4ff3bd6e88e2
geethagrace <101448322+geethagrace@users.noreply.github.com>
2022-04-21 18:42:04 +0530

    Delete inc directory
    updated

--- inc/timer_delay.h
+++
@@ -1,6 +0,0 @@
-#ifndef _timer_delay_h_
-#define _timer_delay_h_
-
-void user_delay();
-
-#endif // _timer_delay_h_
--- inc/usart.h
+++
@@ -1,9 +0,0 @@
-#ifndef _usart_h_
-#define _usart_h_
-
-void USARTInit(uint16_t );
-char USARTReadChar();
-void USARTWriteChar(char);
-void USARTSendString(char *str)
-  
-#endif // _usart_h_
